24f32e25691e927855b3bb6ee4d7d45ab4378dee
Sofia Tété <sofia.garcia.nunes@gamil.com>
2023-10-06 13:17:39 +0100

    agora já não digo

--- Homework2/Homework2.py
+++ Homework2/Homework2.py
@@ -19,33 +19,23 @@ X = df.drop('class', axis=1) #variables
 y = df['class'] #target
 
 
+##### EXERCÍCIO 1 ##########################################
+
+### a) ###
+
 # Fazer Stratified Cross Validation
 cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
-# não sei se é para por shuffle True ou False
-
-# Inicializar o classificador
-#clf = RandomForestClassifier()
-clf = DecisionTreeClassifier()
-
-# Fazer a cross-validation
-scores = cross_val_score(clf, X, y, cv=cv)
-
-print(scores)
 
 # Inicializar os classificadores
 knn = KNeighborsClassifier(n_neighbors=5)
 nb = GaussianNB()
 
 # Fazer a cross-validation
-scores_knn = cross_val_score(knn, X, y, cv=cv)
-scores_nb = cross_val_score(nb, X, y, cv=cv)
+scores_knn = cross_val_score(knn, X, y, cv=cv, scoring='accuracy')
+scores_nb = cross_val_score(nb, X, y, cv=cv, scoring='accuracy')
 
 # Por os scores numa lista com 2 arrays (um para cada classificador)
 scores_list = [scores_knn, scores_nb]
-
-##### EXERCÍCIO 1 ##########################################
-
-### a) ###
 
 # Criar um gráfico com boxplots
 fig, ax = plt.subplots()
@@ -65,8 +55,11 @@ t_stat, p_value = ttest_rel(scores_knn, scores_nb, alternative='greater')
 print('t-statistic:', t_stat)
 print('p-value:', p_value)
 
+# P-value é menor que 0.05 pelo que a hipótese não é verdadeira
+
 ##### EXERCÍCIO 2 ##########################################
 
-
+knn1 = KNeighborsClassifier(n_neighbors=1, weights='uniform', metric='euclidean')
+knn5 = KNeighborsClassifier(n_neighbors=5, weights='uniform', metric='euclidean')
 
 ##### EXERCÍCIO 3 ##########################################
